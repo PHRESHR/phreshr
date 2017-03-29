fe68e179e032d93bda9b49203a22cdbf8009e24b
DeAndre Johnson <dre@phreshr.com>
2017-03-29 17:33:41 -0400

    testing auth with Auth0

---
+++ ui/Auth/auth0-variables.ts
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'phreshr.auth0.com',
+  clientId: 'j3pmDdwu0lm0llytEVxiX0pCiwQpNTzu',
+  apiUrl: 'https://phreshr.auth0.com/api/v2/',
+  callbackUrl: 'http://localhost:8080/callback',
+};
