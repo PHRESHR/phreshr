12fa1487caa2f4b956efba4f99ea0bf89b4174d6
DeAndre Johnson <dre@phreshr.com>
2017-03-30 15:09:04 -0400

    added loading spinner styles

--- ui/components/Loading/Loading.tsx
+++ ui/components/Loading/Loading.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import { branch, renderComponent } from 'recompose';
+import { Spinner } from './styles';
 
 function Loading() {
   return (
-    <div>Loading</div>
+    <Spinner>
+      <div className="sk-cube-grid">
+        <div className="sk-cube sk-cube1" />
+        <div className="sk-cube sk-cube2" />
+        <div className="sk-cube sk-cube3" />
+        <div className="sk-cube sk-cube4" />
+        <div className="sk-cube sk-cube5" />
+        <div className="sk-cube sk-cube6" />
+        <div className="sk-cube sk-cube7" />
+        <div className="sk-cube sk-cube8" />
+        <div className="sk-cube sk-cube9" />
+      </div>
+    </Spinner>
   );
-};
+}
 
 const displayLoadingState = branch(
   props => props.data.loading,
